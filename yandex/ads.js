ae2937a753dbf47f74914b75a20d80b1834e5fc4
danilkazakov <danil.v.kazakov@gmail.com>
2016-08-01 16:15:51 +0300

    обновил index.html

---
+++ yandex/ads.js
@@ -0,0 +1,107 @@
+var ads = [{
+    url: "",
+    title: "Велосипеды Merida в наличии",
+    body: "Велосипеды MERIDA< в наличии! Скидки до 20%! Доставка, сборка 0 р.",
+    domain: "velospec.ru",
+    address: "Большой Полуярославский переулок, 18с4",
+    workingTime: "пн-вс 10:00-22:00",
+    telNum: "+7 (800) 333-82-10",
+    metro: "Чкаловская",
+    sitelinks: [
+        { title: "Велосипеды", url: "" },
+        { title: "Бренды", url: "" },
+        { title: "Скидки и акции", url: "" },
+        { title: "Контакты", url: "" }
+    ],
+    images: [
+        ["//avatars-fast.yandex.net/get-direct/eC8yaF8zgwmHsoRi1dm5RQ/x90", "90", "69"],
+        ["//avatars-fast.yandex.net/get-direct/eC8yaF8zgwmHsoRi1dm5RQ/y90", "118", "90"],
+        ["//avatars-fast.yandex.net/get-direct/eC8yaF8zgwmHsoRi1dm5RQ/x80", "80", "61"],
+        ["//avatars-fast.yandex.net/get-direct/eC8yaF8zgwmHsoRi1dm5RQ/y80", "105", "80"],
+        ["//avatars-fast.yandex.net/get-direct/eC8yaF8zgwmHsoRi1dm5RQ/y150", "196", "150"],
+    ]
+}, {
+    url: "",
+    title: "Квартиры от 1,4 млн в Подмосковье",
+    body: "5 км от МКАД г.Видное. Сдача декабрь 2016г.",
+    domain: "granelle.ru",
+    punyDomain: "granelle.ru",
+    address: "Тверская, д. 22/2, корп. 1",
+    workingTime: "пн-пт 9:00-22:00",
+    telNum: "+7 (495) 281-81-81",
+    sitelinks: [
+        { title: "квартиры в Видном", url: "" },
+        { title: "квартиры в Королёве", url: "" },
+        { title: "квартиры в Мытищах", url: "" },
+        { title: "наши cкидки", url: "" }
+    ],
+    images: [
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/x90", "90", "68"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/y90", "120", "90"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/x80", "80", "60"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/y80", "106", "80"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/y150", "200", "150"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/x160", "160", "120"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/y160", "213", "160"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/x180", "180", "135"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/y180", "239", "180"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/y300", "399", "300"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/x450", "450", "338"],
+        ["//avatars-fast.yandex.net/get-direct/qQkhfFjWyH3KIRxOR0uzWA/y450", "599", "450"]
+    ]
+}, {
+    url: "",
+    title: "Полотенцесушители Грота Москва",
+    body: "Полотенцесушители в наличии. Водяные и электрические с доставкой.",
+    domain: "grota.ru",
+    workingTime: "пн-пт 10:00-18:00",
+    telNum: "+7 (495) 710-79-80",
+    age: "",
+    warning: "",
+    sitelinks: [
+        { title: "каталог полотенцесушители", url: "" },
+        { title: "каталог шкафы", url: "" }
+    ],
+    images: [
+        ["//avatars-fast.yandex.net/get-direct/ty2jMTmp_YQTVMm2FEveXA/x90", "90", "113"],
+        ["//avatars-fast.yandex.net/get-direct/ty2jMTmp_YQTVMm2FEveXA/y90", "72", "90"],
+        ["//avatars-fast.yandex.net/get-direct/ty2jMTmp_YQTVMm2FEveXA/x80", "80", "100"],
+        ["//avatars-fast.yandex.net/get-direct/ty2jMTmp_YQTVMm2FEveXA/y80", "64", "80"],
+        ["//avatars-fast.yandex.net/get-direct/ty2jMTmp_YQTVMm2FEveXA/y150", "120", "150"],
+    ]
+}, {
+    url: "",
+    title: "Нож «Невидимка» скрытого ношения.",
+    body: "4 в 1 = Нож, ремень, пилка, открывалка. Кардшарп в подарок. Закажите!",
+    domain: "hit-prodazh.net",
+    workingTime: "пн-вс 11:00-20:00",
+    telNum: "+7 (909) 920-28-28",
+    warning: "Конструктивно сходные с оружием изделия",
+    sitelinks: [
+        { title: "Для туризма", url: "" },
+        { title: "Для рыбалки", url: "" },
+        { title: "Для самообороны", url: "" },
+        { title: "Для выживания", url: "" }
+    ],
+    images: [
+        ["//avatars-fast.yandex.net/get-direct/_DwrSiAYqNSPYmd-dl5hdQ/x90", "90", "84"],
+        ["//avatars-fast.yandex.net/get-direct/_DwrSiAYqNSPYmd-dl5hdQ/y90", "96", "90"],
+        ["//avatars-fast.yandex.net/get-direct/_DwrSiAYqNSPYmd-dl5hdQ/x80", "80", "75"],
+        ["//avatars-fast.yandex.net/get-direct/_DwrSiAYqNSPYmd-dl5hdQ/y80", "85", "80"],
+        ["//avatars-fast.yandex.net/get-direct/_DwrSiAYqNSPYmd-dl5hdQ/y150", "160", "150"],
+    ]
+}, {
+    url: "",
+    title: "«Посвященный Дню защиты детей»",
+    body: "Ищете билеты на концерт «Посвященный Дню защиты детей» 1 июня? От 990 руб.",
+    domain: "ticketland.ru",
+    workingTime: "пн-вс 9:00-21:00",
+    telNum: "+7 (495) 937-77-37",
+    age: "18+",
+    warning: "",
+    sitelinks: [
+        { title: "1 июня, 14:00", url: "" },
+        { title: "Центр Павла Слободкина", url: "" },
+        { title: "Билеты от 990 до 1210 руб.", url: "" },
+    ]
+}];
